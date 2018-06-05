50ec5fd9332d9022c1135b69b1d255f84a2d21a2
Taufik Hidayat <ovick_kd@yahoo.com>
2018-06-05 11:38:21 +0700

    github-login

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const router = express.Router()
+const { LoginGithub } = require('../controllers/user_controller')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+  res.send('respond with a resource')
+})
 
-module.exports = router;
+router.post('/', LoginGithub)
+
+module.exports = router
